99ab7dc61c6f2bed86b7d97011a1cb6ffb78bc52
gtwar <gtwar@LAPTOP>
2025-10-21 09:13:13 +0200

    Dodaj pliki projektów.

---
+++ Projekt 1A/Projekt 1A.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+// Klasa węzła listy
+template<typename T>
+class Node {
+public:
+    T data;
+    Node* prev;
+    Node* next;
+
+    Node(T value) {
+        data = value;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+// Iterator listy
+template<typename T>
+class ListIterator {
+    Node<T>* current;
+public:
+    ListIterator(Node<T>* node) : current(node) {}
+
+    bool hasNext() { return current && current->next; }
+    bool hasPrev() { return current && current->prev; }
+
+    void next() { if (hasNext()) current = current->next; }
+    void prev() { if (hasPrev()) current = current->prev; }
+
+    T get() { return current->data; }
+};
+
+// Klasa listy dwukierunkowej
+template<typename T>
+class DoublyLinkedList {
+    Node<T>* head;
+    Node<T>* tail;
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    };
+
+    // Dodaj element na początek listy
+    void push_front(T value) {
+        Node<T>* node = new Node<T>(value);
+        node->next = head;
+        if (head) head->prev = node;
+        head = node;
+        if (!tail) tail = node;
+    }
+
+};
+
+int main() {
+
+}
